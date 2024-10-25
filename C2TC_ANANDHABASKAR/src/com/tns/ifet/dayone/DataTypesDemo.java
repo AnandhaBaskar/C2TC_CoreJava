e2f05a2bd540bb46f7d186e41870c75cc19378b3
AnandhaBaskar <anandhabaskar465@gmail.com>
2024-10-25 15:16:27 +0530

    Upload code

---
+++ C2TC_ANANDHABASKAR/src/com/tns/ifet/dayone/DataTypesDemo.java
@@ -0,0 +1,21 @@
+package com.tns.ifet.dayone;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		int value1=9/2; //integer division
+		float value2=101/61;
+		double value3=10d/6f;
+		float value4=5/2; //float division
+		System.out.println("value 1="+value1); 
+		System.out.println("value 2="+value2);
+		System.out.println("value 3="+value3);
+		System.out.println("value 4="+value4);
+		int marker=512;
+		double percentage=marker*0.50f;
+		System.out.println("percentage :" + percentage);
+
+
+	}
+
+}
--- C2TC_ANANDHABASKAR/src/com/tns/ifet/dayone/IndentiferDemo.java
+++ C2TC_ANANDHABASKAR/src/com/tns/ifet/dayone/IndentiferDemo.java
@@ -3,9 +3,9 @@ package com.tns.ifet.dayone;
 public class IndentiferDemo {
 
 	public static void main(String[] args) {
-		int $number = 20; //starts with $
+		int $number = 21; //starts with $
 		System.out.println("value of the number variable is : "+ $number);
-		String studentName="Aniket"; //use camelcase
+		String studentName="Anandha Baskar"; //use camelcase
 		System.out.println("value is : "+studentName);
 
 	}
