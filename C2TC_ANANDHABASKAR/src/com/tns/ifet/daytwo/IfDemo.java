113ad7b708c75301b840847836e16c6ea97f918b
AnandhaBaskar <anandhabaskar465@gmail.com>
2024-10-26 12:22:50 +0530

    newly uploaded code

--- C2TC_ANANDHABASKAR/src/com/tns/ifet/daytwo/IfDemo.java
+++ C2TC_ANANDHABASKAR/src/com/tns/ifet/daytwo/IfDemo.java
@@ -1,7 +1,7 @@
 package com.tns.ifet.daytwo;
 
-public class IfDemo {
-
+public class IfDemo 
+{
 	public static void main(String[] args)
 	{
 		int x=14;
@@ -9,9 +9,6 @@ public class IfDemo {
 			System.out.println("Value of x is not 14");
 		if (x==14)
 			System.out.println("Value of x is equal to 14");
-
-		
-
 	}
 
 }
