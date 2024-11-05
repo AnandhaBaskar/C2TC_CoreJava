370d63fb14a69fa6eb55f044004e3ec36e1620d9
AnandhaBaskar <anandhabaskar465@gmail.com>
2024-11-05 20:16:56 +0530

    uploaded code

---
+++ C2TC_ANANDHABASKAR/src/com/tns/ifet/daythree/inheritance/singleinheritance/SLInheritanceDemo.java
@@ -0,0 +1,15 @@
+package com.tns.ifet.daythree.inheritance.singleinheritance;
+
+public class SLInheritanceDemo {
+
+	public SLInheritanceDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		Student student = new Student("Gayatri", "8080808080", "Bangalore", 9888878787L, 10, "City College");
+				System.out.println(student);
+
+	}
+
+}
